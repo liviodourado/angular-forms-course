47dd8285a11940fc306942907899e317b4228354
Livio Dourado <liviodourado@gmail.com>
2024-04-18 16:11:37 -0300

    feat: Template Driven Forms

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,21 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { Form } from "@angular/forms";
 
 @Component({
-  selector: 'login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
+  val = {
+    email: "hello@gmail.com",
+    password: "12345",
+  };
 
+  constructor() {}
 
-  constructor() {
+  ngOnInit() {}
 
-
+  login(form: Form, event: any) {
+    console.log(form);
+    console.log(event);
   }
-
-  ngOnInit() {
-
-  }
-
 }
