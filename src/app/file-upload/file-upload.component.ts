90b00a556d681b751435c6fda643b430c6dc0b7f
Livio Dourado <liviodourado@gmail.com>
2024-07-09 16:16:30 -0300

    feat: basic file upload

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,16 +1,44 @@
-import {Component, Input} from '@angular/core';
-import {HttpClient, HttpEventType} from '@angular/common/http';
-import {catchError, finalize} from 'rxjs/operators';
-import {AbstractControl, ControlValueAccessor, NG_VALIDATORS, NG_VALUE_ACCESSOR, Validator} from '@angular/forms';
-import {noop, of} from 'rxjs';
-
+import { HttpClient } from "@angular/common/http";
+import { Component, Input } from "@angular/core";
+import { of } from "rxjs";
+import { catchError } from "rxjs/operators";
 
 @Component({
-  selector: 'file-upload',
+  selector: "file-upload",
   templateUrl: "file-upload.component.html",
-  styleUrls: ["file-upload.component.scss"]
+  styleUrls: ["file-upload.component.scss"],
 })
 export class FileUploadComponent {
+  @Input()
+  requiredFileType: string;
 
+  fileName = "";
 
+  fileUploadError = false;
+
+  constructor(private http: HttpClient) {}
+
+  onFileSelected(event) {
+    const file: File = event.target.files[0];
+
+    if (file) {
+      this.fileName = file.name;
+
+      const formData = new FormData();
+
+      formData.append("thumbnail", file);
+
+      this.fileUploadError = false;
+
+      this.http
+        .post("/api/thumbnail-upload", formData)
+        .pipe(
+          catchError((error) => {
+            this.fileUploadError = true;
+            return of(error);
+          })
+        )
+        .subscribe();
+    }
+  }
 }
